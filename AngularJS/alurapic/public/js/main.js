1e166ccb2bbaf1454a780a432b556396755f753e
Rodrigo Nappi <rodrigo.nappi@engage.bz>
2016-10-04 13:03:11 -0300

    Aula 06

--- AngularJS/alurapic/public/js/main.js
+++ AngularJS/alurapic/public/js/main.js
@@ -1,4 +1,27 @@
 // é necessário passar o parametro '[]' para criar um módulo
 // este array '[]' serve para informar ao Angular quais são as dependências deste módulo
 // essas dependências são outros módulos que são utilizados pelo 'alurapic'
-angular.module("alurapic", ['minhasDiretivas']);
+angular.module("alurapic", ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+.config(function($routeProvider, $locationProvider){
+	
+	// configura o Angular para retirar o # das rotas e utilizar o history API do HTML5
+	// o backend deve estar preparado para que este recurso funcione
+	// o browser também de ter suporte a esta API do HTML5
+	$locationProvider.html5Mode(true);
+
+	// routeProvider é um objeto injeta pelo ngRoute
+	// é com ele que conseguimos configurar as rotas
+	$routeProvider.when('/fotos', {
+		templateUrl: 'partials/principal.html',
+		controller: 'FotosController'
+	});
+
+	$routeProvider.when('/fotos/new', {
+		templateUrl: 'partials/foto.html'
+	});
+
+	// rota padrão da aplicação
+	// caso o usuário tente acessar um rota que não existe, ele vai ser redirecionado para essa
+	$routeProvider.otherwise({ redirectTo: '/fotos' });
+
+});
