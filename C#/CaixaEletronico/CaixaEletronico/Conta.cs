afbc94142be012c72bc909cd929f07db6f265358
Rodrigo Nappi <rodrigo.nappi@engage.bz>
2016-10-17 08:22:52 -0200

    C# - Fundamentos da linguagem

---
+++ C#/CaixaEletronico/CaixaEletronico/Conta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico
+{
+    class Conta
+    {
+        public int numero;
+        public string titutlar;
+        public double saldo;
+        public Cliente cliente;
+
+        public void Saca(double ValorASerSacado)
+        {
+            this.saldo -= ValorASerSacado;
+        }
+
+        public void Deposita(double ValorASerDepositado)
+        {
+            this.saldo += ValorASerDepositado;
+        }
+
+        public void Transfere(double valor, Conta destino)
+        {
+            this.Saca(valor);
+            destino.Deposita(valor);
+        }
+    }
+}
